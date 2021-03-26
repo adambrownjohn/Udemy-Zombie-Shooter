f4072b3277797efec4e4aa29fb6ce7e5ef9c4d16
adambrownjohn <80825282+adambrownjohn@users.noreply.github.com>
2021-03-26 17:45:09 +0000

    Add files via upload
    First iteration of raycasting code for weapons

---
+++ Weapons.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons : MonoBehaviour
+{
+  
+    [SerializeField] Camera fpCamera; // field for declaring origin (player camera)
+    [SerializeField] float range = 100f; //range variable for raycasting
+
+
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")) // Bind for shooting
+        {
+            ShootGun(); // method for shooting
+        }
+    }
+
+    private void ShootGun() // method uses raycasting
+    {
+        RaycastHit hit; //new raycast variable 
+        Physics.Raycast(fpCamera.transform.position, fpCamera.transform.forward, out hit, range); //raycast code, delcaring origin with fpCamera variable
+                                                                                                //then moving froward from fpCamera, then "out", then raycast "hit" variable, then "range" variable
+        Debug.Log("I hit: " + hit.transform.name); // test code for printing name of object on raycast hit
+    }
+}
