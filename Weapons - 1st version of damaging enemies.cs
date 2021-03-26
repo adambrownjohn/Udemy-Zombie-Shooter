f4ae341bab712ace5f23a064ca7e49b038941757
adambrownjohn <80825282+adambrownjohn@users.noreply.github.com>
2021-03-26 18:25:30 +0000

    Add files via upload
    First version that allows damage to affect enemies

---
+++ Weapons - 1st version of damaging enemies.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons : MonoBehaviour
+{
+  
+    [SerializeField] Camera fpCamera; 
+    [SerializeField] float range = 100f; 
+    [SerializeField] float damage = 35f; // Damage variable is defined here
+
+
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")) 
+        {
+            ShootGun(); 
+        }
+    }
+
+    private void ShootGun() 
+    {
+        
+        RaycastHit hit; 
+        if (Physics.Raycast(fpCamera.transform.position, fpCamera.transform.forward, out hit, range))
+        {
+            Debug.Log("I hit: " + hit.transform.name); 
+            // TODO: add some effects for more visual spectacle
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            // call a method on EnemyHealth that decreases the enemy health
+            if (target == null) return;
+            target.TakeDamage(damage); //calls on the public "TakeDamage" method in EnemyHealth script, works as a property of the target hit by raycasting, parses in "damage" variable 
+        }
+        else
+        {
+            return;
+        }                                                                                              
+       
+    }
+}
